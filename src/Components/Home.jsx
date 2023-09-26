58791a882f0c4cf0ed4a0b79b7c07eab1e751c53
kajal1801 <anshikajais2001@gmail.com>
2023-09-27 00:21:23 +0530

    First page done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,41 +1,46 @@
 import { FaTwitter, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import Navbar from './Navbar'
 
 const Home = () => {
   return (
-    <div className="home">
-    <div className="upper-right"></div>
-    <div className="lower-left"></div>
-      <p className="preface">Hello!</p>
-      <div className="home-heading">Dynamic Web Developer</div>
-      <div className="home-heading">Sparking Passion Through </div>
-      <div className="tag">Problem - Solving <div className="tagline"></div></div>
-      <div className="tag"><div className="tagline"></div> Community Building</div>
-      <p className="preface">
-        I&apos;m Kajal, an undergraduate student from Kolkata. I&apos;m an upcoming SDE Intern at Amazon. While my main focus is on Frontend Development, I&apos;m also exploring opportunities in Backend Development.
-      </p>
-      <div className="btn-container">
-        <a href="https://drive.google.com/file/d/1L1GxrdXWSJ5EtaVyWZRxzpTY31JI2Xjx/view?usp=sharing" download="Kajal_Jaiswal_Resume" rel="noreferrer" target="_blank">
-        <button className="main-btn" id="resume">
-          <div>My Resume</div>
-          <div className="sideline">
-            <span className="line" id="line11"></span>
-            <span className="line" id="line21"></span>
-            <span className="line" id="line31"></span>
-          </div>
-        </button>
-        </a>
+    <>
+      <Navbar/>
+      <div className="home">
+      <div className="home-content">
+        {/* <div className="upper-right"></div> */}
+        <div className="lower-left"></div>
+        <p className="tag">Hello! I&apos;m Kajal</p>
+        <div className="home-heading">Dynamic Web Developer</div>
+        <div className="home-heading">Sparking Passion Through </div>
+        <div className="tag">Problem - Solving <span id="and">&</span> Community Building</div>
+        <p className="preface">
+          I&apos;m an undergraduate student from Kolkata. I&apos;m an upcoming SDE Intern at Amazon. While my main focus is on Frontend Development, I&apos;m also exploring opportunities in Backend Development.
+        </p>
+        <div className="btn-container">
+          <a href="https://drive.google.com/file/d/1L1GxrdXWSJ5EtaVyWZRxzpTY31JI2Xjx/view?usp=sharing" download="Kajal_Jaiswal_Resume" rel="noreferrer" target="_blank">
+            <button className="main-btn" id="resume">
+              <div>My Resume</div>
+              <div className="sideline">
+                <span className="line" id="line11"></span>
+                <span className="line" id="line21"></span>
+                <span className="line" id="line31"></span>
+              </div>
+            </button>
+          </a>
 
-        <button className="main-btn" id="connect">
-          <div>Let&apos;s Connect</div>
-          <div className="sideline">
-            <span className="sideline-internal"><span className="line" id="line12"></span><span className="icons"><a href="https://www.x.com/kajal_1801" rel="noreferrer" target="_blank"><FaTwitter /></a></span></span>
-            <span className="sideline-internal"><span className="line" id="line22"></span><span className="icons"><a href="https://www.linkedin.com/in/kajal1801" rel="noreferrer" target="_blank"><FaLinkedin /></a></span></span>
-            <span className="sideline-internal"><span className="line" id="line32"></span><span className="icons"><a href="https://github.com/kajal1801" rel="noreferrer" target="_blank"><FaGithub /></a></span></span>
-            <span className="icons"><a href="#"><FaEnvelope /></a></span>
-          </div>  
-        </button>
+          <button className="main-btn" id="connect">
+            <div>Let&apos;s Connect</div>
+            <div className="sideline">
+              <span className="sideline-internal"><span className="line" id="line12"></span><span className="icons"><a href="https://www.x.com/kajal_1801" rel="noreferrer" target="_blank"><FaTwitter /></a></span></span>
+              <span className="sideline-internal"><span className="line" id="line22"></span><span className="icons"><a href="https://www.linkedin.com/in/kajal1801" rel="noreferrer" target="_blank"><FaLinkedin /></a></span></span>
+              <span className="sideline-internal"><span className="line" id="line32"></span><span className="icons"><a href="https://github.com/kajal1801" rel="noreferrer" target="_blank"><FaGithub /></a></span></span>
+              <span className="icons"><a href="#"><FaEnvelope /></a></span>
+            </div>
+          </button>
+        </div>
       </div>
-    </div>
+      </div>
+    </>
   )
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+
+const Navbar = () => {
+    return (
+        <div id="navbar">
+            <div className="navbar-content">
+                <p id="username">kajal1801.</p>
+                <div className="nav-menu">
+                    <a href="#" className="hover-underline-animation">About</a>
+                    <a href="#" className="hover-underline-animation">Work</a>
+                    <a href="#" className="hover-underline-animation">Volunteering</a>
+                    <a href="#" className="hover-underline-animation">Socials</a>
+                </div>
+                <div className="nav-btn">
+                    <button className="main-btn" id="resume">
+                        <div>Let&apos;s connect</div>
+                        <div className="sideline">
+                            <span className="line" id="line11"></span>
+                            <span className="line" id="line21"></span>
+                            <span className="line" id="line31"></span>
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
